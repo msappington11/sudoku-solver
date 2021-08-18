ea9edb91503d2cfba3212eaf0d7033e51e7c90c0
msappington11 <71204360+msappington11@users.noreply.github.com>
2021-08-18 16:49:24 -0500

    Add files via upload

--- sudoku.py
+++ sudoku.py
@@ -55,16 +55,5 @@ def solve(grid):
     solveSquare(first_zero[0][0], first_zero[0][1], grid, isDone)
     return np.count_nonzero(grid) == 81
 
-Sudoku = np.array([[0, 0, 0, 2, 7, 3, 9, 0, 5],
-                   [5, 0, 0, 0, 0, 9, 0, 3, 7],
-                   [7, 9, 0, 4, 0, 0, 0, 0, 2],
-                   [0, 8, 0, 5, 2, 6, 4, 0, 0],
-                   [1, 6, 5, 8, 0, 0, 0, 0, 0],
-                   [0, 0, 2, 0, 9, 0, 5, 0, 6],
-                   [0, 0, 1, 0, 0, 5, 3, 6, 0],
-                   [9, 3, 8, 0, 6, 2, 0, 0, 0],
-                   [0, 0, 0, 9, 3, 0, 0, 2, 8]])
-solve(Sudoku)
-print(time.time() - startTime)
 
 
