bb945b4dfcaef57063e69489402caa37d49f4f1d
msappington11 <71204360+msappington11@users.noreply.github.com>
2021-08-18 16:48:56 -0500

    Add files via upload

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,6 @@
-# written in c++ first then converted over
 import time
-
 import numpy as np
-
+startTime = time.time()
 
 def checkRow(row, grid, num):
     for i in range(9):
@@ -45,75 +43,28 @@ def solveSquare(row, col, grid, isDone):
                     solveSquare(row+1, 0, grid, isDone)
         if(not isDone[0]):
             grid[row][col] = 0 # only do if its not done and keep track if its tracked all the way back mayvbe
-        #if(grid[firstRow][firstCol] == 0):
-            #isDone[0] = True
     else: # if the current square is not blank, calls the next square
         if (col < 8):  # if the current position is not at the end of the row, calls itself on the square to the right
             solveSquare(row, col + 1, grid, isDone)
         elif (row < 8):  # if the current position is at the end of the row, calls itself at the beginning of the next row
             solveSquare(row + 1, 0, grid, isDone)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    """if(grid[row][col] == 0):
-        for i in range(1, 10, 1):
-            if((row == 8 and col == 8 and grid[8][8] !=0) or isDone[0] == True):
-                isDone[0] = True
-                return
-            if(isValid(row, col, grid, i)):
-                grid[row][col] = i
-                if ((row == 8 and col == 8 and grid[8][8] != 0)):
-                    isDone[0] = True
-                    return
-                if(col < 8):
-                    solveSquare(row, col+1, grid, isDone)
-                    if((row == 8 and col == 8 and grid[8][8] !=0) or isDone[0] == True):
-                        isDone[0] = True
-                        return
-                    grid[row][col] = 0
-                elif(row < 8):
-                    solveSquare(row+1, 0, grid, isDone)
-                    if((row == 8 and col == 8 and grid[8][8] !=0) or isDone[0] == True):
-                        isDone[0] = True
-                        return
-                    grid[row][col] = 0
-    else:
-        if(col < 8):
-            solveSquare(row, col+1, grid, isDone)
-        elif(row < 8):
-            solveSquare(row+1, 0, grid, isDone)"""
-
 def solve(grid):
     isDone = [False]
     first_zero = np.argwhere(grid == 0)
     solveSquare(first_zero[0][0], first_zero[0][1], grid, isDone)
     return np.count_nonzero(grid) == 81
+
+Sudoku = np.array([[0, 0, 0, 2, 7, 3, 9, 0, 5],
+                   [5, 0, 0, 0, 0, 9, 0, 3, 7],
+                   [7, 9, 0, 4, 0, 0, 0, 0, 2],
+                   [0, 8, 0, 5, 2, 6, 4, 0, 0],
+                   [1, 6, 5, 8, 0, 0, 0, 0, 0],
+                   [0, 0, 2, 0, 9, 0, 5, 0, 6],
+                   [0, 0, 1, 0, 0, 5, 3, 6, 0],
+                   [9, 3, 8, 0, 6, 2, 0, 0, 0],
+                   [0, 0, 0, 9, 3, 0, 0, 2, 8]])
+solve(Sudoku)
+print(time.time() - startTime)
+
 
